5985023ff522ec70b0ea4bfeaf35910c5334fc99
Meg Scholl <meghan.e.scholl@gmail.com>
2018-03-17 15:05:05 -0500

    working on Google Auth... Error message in console says, 'This domain (127.0.0.1) is not authorized to run this operation. Add it to the OAuth redirect domains list in the Firebase console -> Auth section -> Sign in method tab.' I will work on this after work

--- js/firebaseKey.js
+++ js/firebaseKey.js
@@ -1,11 +1,11 @@
-// "use strict";
+"use strict";
 
-// function getFirebaseKey() {
-//   return {
-//     apiKey: "AIzaSyCeWuB1W6TUS0vTNj5GUA9OU_SKCSdts80",
-//     authDomain: "reso-capstone.firebaseapp.com",
-//     databaseURL: "https://reso-capstone.firebaseio.com",
-//   };
-// }
+function getFirebaseKey() {
+  return {
+    apiKey: "AIzaSyCeWuB1W6TUS0vTNj5GUA9OU_SKCSdts80",
+    authDomain: "reso-capstone.firebaseapp.com",
+    databaseURL: "https://reso-capstone.firebaseio.com"
+  };
+}
 
-// module.exports = getFirebaseKey;
+module.exports = getFirebaseKey;
