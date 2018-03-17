4ec86ca6d77ec2c1ad8355f45dcac9c1ca56ab65
Meg Scholl <meghan.e.scholl@gmail.com>
2018-03-16 22:07:38 -0500

    working to complete the HTML & CSS design structure to start working on the functinality of the product.

---
+++ js/firebaseKey.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function getFirebaseKey() {
+  return {
+    apiKey: "AIzaSyCeWuB1W6TUS0vTNj5GUA9OU_SKCSdts80",
+    authDomain: "reso-capstone.firebaseapp.com",
+    databaseURL: "https://reso-capstone.firebaseio.com",
+  };
+}
+
+module.exports = getFirebaseKey;
---
+++ js/googleAuth.js
@@ -0,0 +1,3 @@
+"use strict";
+var provider = new firebase.auth.GoogleAuthProvider();
+
